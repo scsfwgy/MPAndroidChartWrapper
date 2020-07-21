fa19f10f4bf6a8390e29c6199e002c82969ec85e
wgyscsf <wgyscsf@163.com>
2020-07-21 19:59:29 +0800

    首页添加标题

--- app/src/main/java/com/matt/demo/MainActivity.kt
+++ app/src/main/java/com/matt/demo/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.matt.demo
 
 import android.os.Bundle
+import android.view.View
 import com.example.sample_binance.ui.activity.BinListActivity
 import com.matt.libwrapper.ui.base.HandleExceptionActivity
+import com.matt.libwrapper.ui.base.template.Template
+import com.matt.libwrapper.ui.base.template.TemplateBarActivity
 import com.matt.sample_zm.ui.activity.SymbolListActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : HandleExceptionActivity() {
+class MainActivity : TemplateBarActivity() {
 
     private fun initListener() {
         am_b_kview.setOnClickListener {
@@ -17,9 +20,27 @@ class MainActivity : HandleExceptionActivity() {
         }
     }
 
+    override fun templateType(): Int {
+        return Template.TEMPLATETYPE_DEFVIEW
+    }
+
+    override fun addChildrenView(): Any {
+        return R.layout.activity_main
+    }
+
+    override fun renderTitle(): Any {
+        return "MPAndroidChartWrapper Demo"
+    }
+
     override fun onCatchCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_main)
+        super.onCatchCreate(savedInstanceState)
+        initView()
         initListener()
 
+    }
+
+    private fun initView() {
+        getBarContainer().setBackgroundColor(getColor2(R.color.wrapper_theme_design))
+        getLeftImageView().visibility = View.INVISIBLE
     }
 }
