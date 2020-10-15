05aaacb99a396e008de7af3eb3db1a54f5b84212
wgyscsf <wgyscsf@163.com>
2020-10-15 13:27:52 +0800

    微调

--- sample_binance/src/main/java/com/example/sample_binance/SampleBinanceInit.kt
+++ sample_binance/src/main/java/com/example/sample_binance/SampleBinanceInit.kt
@@ -16,7 +16,7 @@ object SampleBinanceInit {
     //不需要翻墙
     const val BASE_URL = "https://api.binancezh.com/"
     const val BASE_URL_WS = "wss://stream.binance.com:9443/stream"
-    //const val BASE_URL_WS = "ws://echo.websocket.org"
+    //const val BASE_URL_WS = "wss://stream.binancezh.com:9443/stream"
 
     fun init(application: Application) {
         BinWsImpl.binWs.conn()
